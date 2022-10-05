67e0c3add4da8af0d3b9653b474e3cdc42a1c06b
Sam Spilsbury <smspillaz@gmail.com>
2022-10-05 23:47:00 +0300

    train_transformer: Script for training a big transformer

---
+++ train_transformer.py
@@ -0,0 +1,488 @@
+import argparse
+import os
+import math
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+import sys
+from torch.utils.data import DataLoader, Subset
+from positional_encodings.torch_encodings import PositionalEncoding1D
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.loggers import TensorBoardLogger
+
+from .dataset import PaddingDataset, ReshuffleOnIndexZeroDataset
+from .embedding import BOWEmbedding
+from .load_data import load_data
+from .logging import LoadableCSVLogger
+from .scheduler import transformer_optimizer_config
+
+
+class StateEncoderTransformer(nn.Module):
+    def __init__(
+        self,
+        n_state_components,
+        input_size,
+        embedding_dim,
+        nlayers,
+        dropout_p,
+        pad_word_idx,
+    ):
+        super().__init__()
+        self.n_state_components = n_state_components
+        self.embedding_dim = embedding_dim
+        self.state_embedding = BOWEmbedding(
+            64, self.n_state_components, self.embedding_dim
+        )
+        self.state_projection = nn.Linear(
+            self.n_state_components * self.embedding_dim, self.embedding_dim
+        )
+        self.embedding = nn.Embedding(input_size, embedding_dim)
+        self.embedding_projection = nn.Linear(embedding_dim * 2, embedding_dim)
+        self.dropout = nn.Dropout(dropout_p)
+        self.encoding = nn.Parameter(torch.randn(embedding_dim))
+        self.pos_encoding = PositionalEncoding1D(embedding_dim)
+        self.pad_word_idx = pad_word_idx
+        self.transformer_encoder = nn.TransformerEncoder(
+            nn.TransformerEncoderLayer(
+                d_model=embedding_dim,
+                nhead=4,
+                dim_feedforward=embedding_dim * 4,
+                dropout=dropout_p,
+            ),
+            num_layers=nlayers,
+        )
+
+    def forward(self, state_padded, z_padded):
+        state_padding_bits = torch.zeros_like(state_padded[..., 0]).bool()
+        z_padding_bits = z_padded == self.pad_word_idx
+
+        state_embed_seq = self.state_projection(self.state_embedding(state_padded))
+
+        z_embed_seq = self.embedding(z_padded)
+        z_embed_seq = torch.cat([self.pos_encoding(z_embed_seq), z_embed_seq], dim=-1)
+        z_embed_seq = self.embedding_projection(z_embed_seq)
+        state_embed_seq = self.dropout(state_embed_seq)
+        z_embed_seq = self.dropout(z_embed_seq)
+
+        z_embed_seq = torch.cat([state_embed_seq, z_embed_seq], dim=1)
+        padding_bits = torch.cat([state_padding_bits, z_padding_bits], dim=-1)
+
+        encoded_seq = self.transformer_encoder(
+            z_embed_seq.transpose(1, 0),
+            src_key_padding_mask=padding_bits,
+        )
+
+        # bs x emb_dim, z_seq_len x bs x emb_dim
+        return encoded_seq, padding_bits
+
+
+class DecoderTransformer(nn.Module):
+    def __init__(
+        self,
+        n_state_components,
+        hidden_size,
+        output_size,
+        nlayers,
+        pad_action_idx,
+        dropout_p=0.1,
+    ):
+        #
+        # Input
+        #  hidden_size : number of hidden units in Transformer, and embedding size for output symbols
+        #  output_size : number of output symbols
+        #  nlayers : number of hidden layers
+        #  dropout_p : dropout applied to symbol embeddings and Transformers
+        #
+        super().__init__()
+        self.n_state_components = n_state_components
+        self.nlayers = nlayers
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.dropout_p = dropout_p
+        self.tanh = nn.Tanh()
+        self.embedding = nn.Embedding(output_size, hidden_size)
+        self.embedding_projection = nn.Linear(hidden_size * 2, hidden_size)
+        self.pos_encoding = PositionalEncoding1D(hidden_size)
+        self.dropout = nn.Dropout(dropout_p)
+        self.pad_action_idx = pad_action_idx
+        self.decoder = nn.TransformerDecoder(
+            nn.TransformerDecoderLayer(
+                d_model=hidden_size,
+                dim_feedforward=hidden_size * 4,
+                dropout=dropout_p,
+                nhead=4,
+            ),
+            num_layers=nlayers,
+        )
+        self.out = nn.Linear(hidden_size, output_size)
+
+    def forward(self, inputs, encoder_outputs, encoder_padding):
+        # Run batch decoder forward for a single time step.
+        #
+        # Input
+        #  input: LongTensor of length batch_size x seq_len (left-shifted targets)
+        #  memory: encoder state
+        #
+        # Output
+        #   output : unnormalized output probabilities, batch_size x output_size
+        #
+        # Embed each input symbol
+        input_padding_bits = inputs == self.pad_action_idx
+
+        embedding = self.embedding(inputs)  # batch_size x hidden_size
+        embedding = self.embedding_projection(
+            torch.cat([embedding, self.pos_encoding(embedding)], dim=-1)
+        )
+        embedding = self.dropout(embedding)
+
+        decoded = self.decoder(
+            tgt=embedding.transpose(0, 1),
+            memory=encoder_outputs,
+            memory_key_padding_mask=encoder_padding,
+            tgt_key_padding_mask=input_padding_bits,
+            tgt_mask=torch.triu(
+                torch.full((inputs.shape[-1], inputs.shape[-1]), float("-inf")),
+                diagonal=1,
+            ).to(inputs.device),
+        ).transpose(0, 1)
+
+        return self.out(decoded)
+
+
+class TransformerLearner(pl.LightningModule):
+    def __init__(
+        self,
+        n_state_components,
+        x_categories,
+        y_categories,
+        embed_dim,
+        dropout_p,
+        nlayers,
+        pad_word_idx,
+        pad_action_idx,
+        sos_action_idx,
+        eos_action_idx,
+        lr=1e-4,
+        wd=1e-2,
+        warmup_proportion=0.001,
+        decay_power=-1,
+        predict_steps=64,
+    ):
+        super().__init__()
+        self.encoder = StateEncoderTransformer(
+            n_state_components,
+            x_categories,
+            embed_dim,
+            nlayers,
+            dropout_p,
+            pad_word_idx,
+        )
+        self.decoder = DecoderTransformer(
+            n_state_components,
+            embed_dim,
+            y_categories,
+            nlayers,
+            pad_action_idx,
+            dropout_p,
+        )
+        self.y_categories = y_categories
+        self.pad_word_idx = pad_word_idx
+        self.pad_action_idx = pad_action_idx
+        self.sos_action_idx = sos_action_idx
+        self.eos_action_idx = eos_action_idx
+        self.save_hyperparameters()
+
+    def configure_optimizers(self):
+        return transformer_optimizer_config(
+            self,
+            self.hparams.lr,
+            warmup_proportion=self.hparams.warmup_proportion,
+            weight_decay=self.hparams.wd,
+            decay_power=self.hparams.decay_power,
+        )
+
+    def encode(self, states, queries):
+        return self.encoder(states, queries)
+
+    def decode_autoregressive(self, decoder_in, encoder_outputs, encoder_padding):
+        return self.decoder(decoder_in, encoder_outputs, encoder_padding)
+
+    def forward(self, states, queries, decoder_in):
+        encoded, encoder_padding = self.encoder(states, queries)
+        return self.decode_autoregressive(decoder_in, encoded, encoder_padding)
+
+    def training_step(self, x, idx):
+        query, targets, state = x
+        actions_mask = targets == self.pad_action_idx
+
+        decoder_in = torch.cat(
+            [torch.ones_like(targets)[:, :1] * self.sos_action_idx, targets], dim=-1
+        )
+
+        # Now do the training
+        preds = self.forward(state, query, decoder_in)[:, :-1]
+
+        # Ultimately we care about the cross entropy loss
+        loss = F.cross_entropy(
+            preds.flatten(0, -2),
+            targets.flatten().long(),
+            ignore_index=self.pad_action_idx,
+        )
+
+        argmax_preds = preds.argmax(dim=-1)
+        argmax_preds[actions_mask] = self.pad_action_idx
+        exacts = (argmax_preds == targets).all(dim=-1).to(torch.float).mean()
+
+        self.log("tloss", loss, prog_bar=True)
+        self.log("texact", exacts, prog_bar=True)
+        self.log(
+            "tacc",
+            (preds.argmax(dim=-1)[~actions_mask] == targets[~actions_mask])
+            .float()
+            .mean(),
+            prog_bar=True,
+        )
+
+        return loss
+
+    def validation_step(self, x, idx, dl_idx=0):
+        query, targets, state = x
+        actions_mask = targets == self.pad_action_idx
+
+        decoder_in = torch.cat(
+            [torch.ones_like(targets)[:, :1] * self.sos_action_idx, targets], dim=-1
+        )
+
+        # Now do the training
+        preds = self.forward(state, query, decoder_in)[:, :-1]
+
+        # Ultimately we care about the cross entropy loss
+        loss = F.cross_entropy(
+            preds.flatten(0, -2),
+            targets.flatten().long(),
+            ignore_index=self.pad_action_idx,
+        )
+
+        argmax_preds = preds.argmax(dim=-1)
+        argmax_preds[actions_mask] = self.pad_action_idx
+        exacts = (argmax_preds == targets).all(dim=-1).to(torch.float).mean()
+
+        self.log("vloss", loss, prog_bar=True)
+        self.log("vexact", exacts, prog_bar=True)
+        self.log(
+            "vacc",
+            (preds.argmax(dim=-1)[~actions_mask] == targets[~actions_mask])
+            .float()
+            .mean(),
+            prog_bar=True,
+        )
+
+    def predict_step(self, x, idx, dl_idx=0):
+        instruction, target, state = x[:3]
+
+        encodings, key_padding_mask = self.encode(state, instruction)
+
+        # Recursive decoding, start with a batch of SOS tokens
+        decoder_in = torch.tensor(
+            self.sos_action_idx, dtype=torch.long, device=self.device
+        )[None].expand(instruction.shape[0], 1)
+
+        logits = []
+
+        with torch.no_grad():
+            for i in range(target.shape[1]):
+                logits.append(
+                    self.decode_autoregressive(decoder_in, encodings, key_padding_mask)[
+                        :, -1
+                    ]
+                )
+                decoder_out = logits[-1].argmax(dim=-1)
+                decoder_in = torch.cat([decoder_in, decoder_out[:, None]], dim=1)
+
+            decoded = decoder_in
+            # these are shifted off by one
+            decoded_eq_mask = (
+                (decoded == self.eos_action_idx).int().cumsum(dim=-1).bool()[:, :-1]
+            )
+            decoded = decoded[:, 1:]
+            decoded[decoded_eq_mask] = -1
+            logits = torch.stack(logits, dim=1)
+
+        exacts = (decoded == target).all(dim=-1).cpu().numpy()
+
+        decoded = decoded.cpu().numpy()
+        decoded_select_mask = decoded != -1
+        decoded = [d[m] for d, m in zip(decoded, decoded_select_mask)]
+
+        target = target.cpu().numpy()
+        target = [d[d != -1] for d in target]
+
+        instruction = instruction.cpu().numpy()
+        instruction = [i[i != -1] for i in instruction]
+
+        logits = logits.cpu().numpy()
+        logits = [l[m] for l, m in zip(logits, decoded_select_mask)]
+
+        return tuple([state, instruction, decoded, logits, exacts, target] + x[3:])
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--train-demonstrations", type=str, required=True)
+    parser.add_argument("--valid-demonstrations-directory", type=str, required=True)
+    parser.add_argument("--dictionary", type=str, required=True)
+    parser.add_argument("--seed", type=int, default=0)
+    parser.add_argument("--train-batch-size", type=int, default=64)
+    parser.add_argument("--valid-batch-size", type=int, default=128)
+    parser.add_argument("--batch-size-mult", type=int, default=16)
+    parser.add_argument("--hidden-size", type=int, default=128)
+    parser.add_argument("--nlayers", type=int, default=8)
+    parser.add_argument("--dropout-p", type=float, default=0.0)
+    parser.add_argument("--lr", type=float, default=1e-4)
+    parser.add_argument("--wd", type=float, default=1e-2)
+    parser.add_argument("--warmup-proportion", type=float, default=0.1)
+    parser.add_argument("--decay-power", type=int, default=-1)
+    parser.add_argument("--iterations", type=int, default=2500000)
+    parser.add_argument("--check-val-every", type=int, default=1000)
+    parser.add_argument("--enable-progress", action="store_true")
+    parser.add_argument("--restore-from-checkpoint", action="store_true")
+    parser.add_argument("--version", type=int, default=None)
+    parser.add_argument("--tag", type=str, default="none")
+    args = parser.parse_args()
+
+    exp_name = "gscan"
+    model_name = "transformer_encoder_only_decode_actions"
+    dataset_name = "gscan"
+    effective_batch_size = args.train_batch_size * args.batch_size_mult
+    exp_name = f"{exp_name}_s_{args.seed}_m_{model_name}_it_{args.iterations}_b_{effective_batch_size}_d_gscan_t_{args.tag}"
+    model_dir = f"models/{exp_name}/{model_name}"
+    model_path = f"{model_dir}/{exp_name}.pt"
+    print(model_path)
+    print(
+        f"Batch size {args.train_batch_size}, mult {args.batch_size_mult}, total {args.train_batch_size * args.batch_size_mult}"
+    )
+
+    os.makedirs(model_dir, exist_ok=True)
+
+    if os.path.exists(f"{model_path}"):
+        print(f"Skipping {model_path} as it already exists")
+        return
+
+    seed = args.seed
+    iterations = args.iterations
+
+    pl.seed_everything(seed)
+
+    (
+        (
+            WORD2IDX,
+            ACTION2IDX,
+            color_dictionary,
+            noun_dictionary,
+        ),
+        (train_demonstrations, valid_demonstrations_dict),
+    ) = load_data(
+        args.train_demonstrations, args.valid_demonstrations_directory, args.dictionary
+    )
+
+    IDX2WORD = {i: w for w, i in WORD2IDX.items()}
+    IDX2ACTION = {i: w for w, i in ACTION2IDX.items()}
+
+    pad_word = WORD2IDX["[pad]"]
+    pad_action = ACTION2IDX["[pad]"]
+    sos_action = ACTION2IDX["[sos]"]
+    eos_action = ACTION2IDX["[eos]"]
+
+    train_dataset = ReshuffleOnIndexZeroDataset(
+        PaddingDataset(
+            train_demonstrations,
+            (8, 72, None),
+            (pad_word, pad_action, None),
+        )
+    )
+
+    pl.seed_everything(seed)
+    meta_module = TransformerLearner(
+        7,
+        len(IDX2WORD),
+        len(IDX2ACTION),
+        args.hidden_size,
+        args.dropout_p,
+        args.nlayers,
+        pad_word,
+        pad_action,
+        sos_action,
+        eos_action,
+        lr=args.lr,
+        decay_power=args.decay_power,
+        warmup_proportion=args.warmup_proportion,
+    )
+    print(meta_module)
+
+    train_dataloader = DataLoader(
+        train_dataset,
+        batch_size=args.train_batch_size,
+        pin_memory=True,
+    )
+
+    check_val_opts = {}
+    interval = args.check_val_every / len(train_dataloader)
+
+    # Every check_val_interval steps, regardless of how large the training dataloader is
+    if interval > 1.0:
+        check_val_opts["check_val_every_n_epoch"] = math.floor(interval)
+    else:
+        check_val_opts["val_check_interval"] = interval
+
+    checkpoint_cb = ModelCheckpoint(
+        monitor="vexact/dataloader_idx_0",
+        auto_insert_metric_name=False,
+        save_top_k=5,
+        mode="max",
+    )
+
+    logs_root_dir = f"logs/{exp_name}/{model_name}/{dataset_name}/{seed}"
+    most_recent_version = args.version
+
+    trainer = pl.Trainer(
+        logger=[
+            TensorBoardLogger(logs_root_dir, version=most_recent_version),
+            LoadableCSVLogger(
+                logs_root_dir, version=most_recent_version, flush_logs_every_n_steps=10
+            ),
+        ],
+        callbacks=[pl.callbacks.LearningRateMonitor(), checkpoint_cb],
+        max_steps=iterations,
+        num_sanity_val_steps=10,
+        gpus=1 if torch.cuda.is_available() else 0,
+        precision=16 if torch.cuda.is_available() else None,
+        default_root_dir=logs_root_dir,
+        accumulate_grad_batches=args.batch_size_mult,
+        enable_progress_bar=sys.stdout.isatty() or args.enable_progress,
+        gradient_clip_val=0.2,
+        **check_val_opts,
+    )
+
+    trainer.fit(
+        meta_module,
+        train_dataloader,
+        [
+            DataLoader(
+                PaddingDataset(
+                    Subset(demonstrations, torch.randperm(len(demonstrations))[:1024]),
+                    (8, 72, None),
+                    (pad_word, pad_action, None),
+                ),
+                batch_size=max([args.train_batch_size, args.valid_batch_size]),
+                pin_memory=True,
+            )
+            for demonstrations in valid_demonstrations_dict.values()
+        ],
+    )
+    print(f"Done, saving {model_path}")
+    trainer.save_checkpoint(f"{model_path}")
+
+
+if __name__ == "__main__":
+    main()
