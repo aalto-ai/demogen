2264033de5cbf6ed6f3014fce77de0e2b6bf9ccc
Sam Spilsbury <spilsbt1@puhti-login12.bullx>
2022-10-17 17:17:22 +0300

    Revert "train_vilbert: Put pre-norms before attention and mlp too"
    This reverts commit 06e7ae7a3b5db16e1e162db04cb3983c805baa87.

--- scripts/train_vilbert.py
+++ scripts/train_vilbert.py
@@ -155,16 +155,12 @@ class TransformerMLP(nn.Module):
 class TransformerCrossAttentionLayer(nn.Module):
     def __init__(self, emb_dim, ff_dim, nhead=4, norm_first=False, dropout_p=0.0):
         super().__init__()
-        self.norm_in_x = nn.LayerNorm(emb_dim)
-        self.norm_in_y = nn.LayerNorm(emb_dim)
         self.mha_x_to_y = nn.MultiheadAttention(emb_dim, nhead, dropout=dropout_p)
         self.mha_y_to_x = nn.MultiheadAttention(emb_dim, nhead, dropout=dropout_p)
         self.dense_x_to_y = TransformerMLP(emb_dim, ff_dim, norm_first, dropout_p)
         self.dense_y_to_x = TransformerMLP(emb_dim, ff_dim, norm_first, dropout_p)
 
     def forward(self, x, y, x_key_padding_mask=None, y_key_padding_mask=None):
-        x = self.norm_in_x(x)
-        y = self.norm_in_y(y)
         mha_x, _ = self.mha_x_to_y(x, y, y, key_padding_mask=y_key_padding_mask)
         mha_y, _ = self.mha_y_to_x(y, x, x, key_padding_mask=x_key_padding_mask)
 
@@ -174,12 +170,11 @@ class TransformerCrossAttentionLayer(nn.Module):
 class TransformerIntermediateLayer(nn.Module):
     def __init__(self, emb_dim, ff_dim):
         super().__init__()
-        self.norm = nn.LayerNorm(emb_dim)
         self.linear = nn.Linear(emb_dim, ff_dim)
         self.gelu = nn.GELU()
 
     def forward(self, x):
-        return self.gelu(self.linear(self.norm(x)))
+        return self.gelu(self.linear(x))
 
 
 class TransformerCrossEncoderLayer(nn.Module):
