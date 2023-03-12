451d68627e50ad1a72507d3c1988b1252dc56799
Sam Spilsbury <smspillaz@gmail.com>
2023-03-12 18:41:54 +0200

    scripts/analyze_supports_correctness: Use util.solver imports

--- scripts/analyze_supports_correctness.py
+++ scripts/analyze_supports_correctness.py
@@ -2,217 +2,17 @@ import numpy as np
 import argparse
 
 from gscan_metaseq2seq.util.load_data import load_data, load_data_directories
-from gscan_metaseq2seq.gscan.world import (
-    Situation,
-    ObjectVocabulary,
-    World,
-    INT_TO_DIR,
-    Object,
-    PositionedObject,
-    Position,
-)
-from gscan_metaseq2seq.gscan.vocabulary import Vocabulary
-from gscan_metaseq2seq.util.dataset import MapDataset
-from torch.utils.data import Dataset
-import torch
-import os
 from tqdm.auto import tqdm
-from scipy.stats import hmean
 import itertools
-import json
 import multiprocessing
 import pandas as pd
 
-from typing import Optional
-
-TYPE_GRAMMAR = "adverb"
-INTRANSITIVE_VERBS = "walk"
-TRANSITIVE_VERBS = "pull,push"
-ADVERBS = "cautiously,while spinning,hesitantly,while zigzagging"
-NOUNS = "square,cylinder,circle,box"
-COLOR_ADJECTIVES = "red,green,yellow,blue"
-SIZE_ADJECTIVES = "big,small"
-MIN_OTHER_OBJECTS = 0
-MAX_OBJECTS = 2
-MIN_OBJECT_SIZE = 1
-MAX_OBJECT_SIZE = 4
-GRID_SIZE = 6
-
-
-def create_world(
-    vocabulary,
-    grid_size: Optional[int] = None,
-    min_object_size: Optional[int] = None,
-    max_object_size: Optional[int] = None,
-    type_grammar: Optional[str] = None,
-):
-
-    # Object vocabulary.
-    object_vocabulary = ObjectVocabulary(
-        shapes=vocabulary.get_semantic_shapes(),
-        colors=vocabulary.get_semantic_colors(),
-        min_size=min_object_size or MIN_OBJECT_SIZE,
-        max_size=max_object_size or MAX_OBJECT_SIZE,
-    )
-
-    # Initialize the world.
-    return World(
-        grid_size=grid_size or GRID_SIZE,
-        colors=vocabulary.get_semantic_colors(),
-        object_vocabulary=object_vocabulary,
-        shapes=vocabulary.get_semantic_shapes(),
-        save_directory=None,
-    )
-
-
-def reinitialize_world(
-    world,
-    situation: Situation,
-    vocabulary,
-    mission="",
-    manner=None,
-    verb=None,
-    end_pos=None,
-    required_push=0,
-    required_pull=0,
-    num_instructions=0,
-):
-    objects = []
-    for positioned_object in situation.placed_objects:
-        objects.append((positioned_object.object, positioned_object.position))
-    world.initialize(
-        objects,
-        agent_position=situation.agent_pos,
-        agent_direction=situation.agent_direction,
-        target_object=situation.target_object,
-        carrying=situation.carrying,
-    )
-    if mission:
-        is_transitive = False
-        if verb in vocabulary.get_transitive_verbs():
-            is_transitive = True
-        world.set_mission(
-            mission,
-            manner=manner,
-            verb=verb,
-            is_transitive=is_transitive,
-            end_pos=end_pos,
-            required_push=required_push,
-            required_pull=required_pull,
-            num_instructions=num_instructions,
-        )
-
-    return world
-
-
-def segment_instruction(query_instruction, word2idx, colors, nouns):
-    verb_words = [
-        [word2idx[w] for w in v] for v in [["walk", "to"], ["push"], ["pull"]]
-    ]
-    adverb_words = [
-        [word2idx[w] for w in v]
-        for v in [
-            ["while spinning"],
-            ["while zigzagging"],
-            ["hesitantly"],
-            ["cautiously"],
-        ]
-    ]
-    size_words = [word2idx[w] for w in ["small", "big"]]
-    color_words = [word2idx[w] for w in list(colors)]
-    noun_words = [word2idx[w] for w in list(nouns) if w in word2idx]
-
-    query_verb_words = [
-        v for v in verb_words if all([w in query_instruction for w in v])
-    ]
-    query_adverb_words = [
-        v for v in adverb_words if all([w in query_instruction for w in v])
-    ]
-    query_size_words = [v for v in size_words if v in query_instruction]
-    query_color_words = [v for v in color_words if v in query_instruction]
-    query_noun_words = [v for v in noun_words if v in query_instruction]
-
-    return (
-        query_verb_words,
-        query_adverb_words,
-        query_size_words,
-        query_color_words,
-        query_noun_words,
-    )
-
-
-def find_agent_position(state):
-    return [s for s in state if s[3] != 0][0]
-
-
-def find_target_object(state, size, color, noun, idx2word, idx2color, idx2noun):
-    color_word = [idx2word[c] for c in color]
-    noun_word = [idx2word[c] for c in noun]
-    size_word = [idx2word[c] for c in size]
-
-    # Find any state elements with a matching noun, then
-    # filter by matching color
-    states_with_matching_noun = [
-        s for s in state if s[2] and idx2noun[s[2] - 1] in noun_word
-    ]
-    states_with_matching_color = [
-        s
-        for s in states_with_matching_noun
-        if s[1] and idx2color[s[1] - 1] in color_word or not color_word
-    ]
-    sorted_by_size = sorted(states_with_matching_color, key=lambda x: x[0])
-
-    if not sorted_by_size:
-        return None
-
-    if size_word and size_word[0] == "small":
-        return sorted_by_size[0]
-
-    if size_word and size_word[0] == "big":
-        return sorted_by_size[-1]
-
-    return sorted_by_size[0]
-
-
-def state_to_situation(query_instruction, state, word2idx, colors, nouns):
-    idx2word = [w for w in word2idx if w != word2idx["[pad]"]]
-    verb, adverb, size, color, noun = segment_instruction(
-        query_instruction, word2idx, colors, nouns
-    )
-    agent = find_agent_position(state)
-    target_object = find_target_object(
-        state, size, color, noun, idx2word, colors, nouns
-    )
-    return (
-        [idx2word[w] for w in query_instruction],
-        Situation(
-            grid_size=6,
-            agent_position=Position(agent[-1], agent[-2]),
-            agent_direction=INT_TO_DIR[agent[-3] - 1],
-            target_object=None
-            if target_object is None
-            else PositionedObject(
-                object=Object(
-                    shape=nouns[target_object[2] - 1],
-                    color=colors[target_object[1] - 1],
-                    size=target_object[0],
-                ),
-                position=Position(target_object[-1], target_object[-2]),
-                vector=[],
-            ),
-            placed_objects=[
-                PositionedObject(
-                    object=Object(
-                        shape=nouns[o[2] - 1], color=colors[o[1] - 1], size=o[0]
-                    ),
-                    position=Position(o[-1], o[-2]),
-                    vector=[],
-                )
-                for o in state[1:]
-                if not (o == 0).all()
-            ],
-        ),
-    )
+from gscan_metaseq2seq.util.solver import (
+    create_vocabulary,
+    create_world,
+    state_to_situation,
+    reinitialize_world,
+)
 
 
 def demonstrate_command_oracle(
@@ -325,14 +125,7 @@ def instruction_is_correct(
 def compute_num_correct_and_valid(
     example, word2idx, action2idx, color_dictionary, noun_dictionary, limit_demos
 ):
-    vocab = Vocabulary.initialize(
-        intransitive_verbs=(INTRANSITIVE_VERBS).split(","),
-        transitive_verbs=(TRANSITIVE_VERBS).split(","),
-        adverbs=(ADVERBS).split(","),
-        nouns=(NOUNS).split(","),
-        color_adjectives=(COLOR_ADJECTIVES).split(","),
-        size_adjectives=(SIZE_ADJECTIVES).split(","),
-    )
+    vocab = create_vocabulary()
     world = create_world(vocab)
 
     (
