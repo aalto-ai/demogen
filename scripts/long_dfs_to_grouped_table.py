0ce43ffebff19daaaa6649140e674981fddc088a
Sam Spilsbury <smspillaz@gmail.com>
2023-10-21 14:36:56 +0300

    long_dfs_to_grouped_table: Mean of means

--- scripts/long_dfs_to_grouped_table.py
+++ scripts/long_dfs_to_grouped_table.py
@@ -5,6 +5,11 @@ import matplotlib.pyplot as plt
 import os
 from tqdm.auto import tqdm
 
+def concat_seed(df, i):
+    df['seed'] = i
+    print(df.columns)
+    return df
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -12,18 +17,27 @@ def main():
     args = parser.parse_args()
 
     df = pd.concat([
-        pd.read_csv(csv)
-        for csv in tqdm(args.csvs)
+        concat_seed(pd.read_csv(csv), i)
+        for i, csv in enumerate(tqdm(args.csvs))
     ]).reset_index(drop=True).drop("Unnamed: 0", axis=1)
     df = df.loc[df[df.columns[1]] == "h"]
-    mean = df.groupby(df.columns[0]).agg({
+    means = df.groupby(['seed', df.columns[0]]).agg({
+        df.columns[0]: "first",
         df.columns[1]: "first",
         df.columns[2]: "mean"
-    }).round(2).astype(str)
-    std = df.groupby(df.columns[0]).agg({
+    }).reset_index(drop=True)
+    mean = means.groupby(df.columns[0]).agg({
+        df.columns[0]: "first",
+        df.columns[1]: "first",
+        df.columns[2]: "mean"
+    }).reset_index(drop=True).round(2).astype(str)
+    std = means.groupby(df.columns[0]).agg({
+        df.columns[0]: "first",
         df.columns[1]: "first",
         df.columns[2]: "std"
-    }).round(2).astype(str)
+    }).reset_index(drop=True).round(2).astype(str)
+
+    mean[mean.columns[2]] = mean[mean.columns[2]] + " ± " + std[std.columns[2]]
 
     print(mean.to_latex())
 
