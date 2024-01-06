7c5bafdfa6ba4987ced3494d353bd63725b88c6c
Sam Spilsbury <smspillaz@gmail.com>
2024-01-06 22:38:34 +0200

    scripts/train_transformer_images: Add EMA support

--- scripts/train_transformer_images.py
+++ scripts/train_transformer_images.py
@@ -1,4 +1,5 @@
 import argparse
+import copy
 import os
 import math
 
@@ -278,6 +279,124 @@ class ImageRenderingDemonstrationsDataset(Dataset):
         return len(self.train_demonstrations)
 
 
+class ModelEmaV2(nn.Module):
+    """ Model Exponential Moving Average V2
+
+    Keep a moving average of everything in the model state_dict (parameters and buffers).
+    V2 of this module is simpler, it does not match params/buffers based on name but simply
+    iterates in order. It works with torchscript (JIT of full model).
+
+    This is intended to allow functionality like
+    https://www.tensorflow.org/api_docs/python/tf/train/ExponentialMovingAverage
+
+    A smoothed version of the weights is necessary for some training schemes to perform well.
+    E.g. Google's hyper-params for training MNASNet, MobileNet-V3, EfficientNet, etc that use
+    RMSprop with a short 2.4-3 epoch decay period and slow LR decay rate of .96-.99 requires EMA
+    smoothing of weights to match results. Pay attention to the decay constant you are using
+    relative to your update count per epoch.
+
+    To keep EMA from using GPU resources, set device='cpu'. This will save a bit of memory but
+    disable validation of the EMA weights. Validation will have to be done manually in a separate
+    process, or after the training stops converging.
+
+    This class is sensitive where it is initialized in the sequence of model init,
+    GPU assignment and distributed training wrappers.
+    """
+    def __init__(self, model, decay=0.9999, device=None):
+        super(ModelEmaV2, self).__init__()
+        # make a copy of the model for accumulating moving average of weights
+        self.module = copy.deepcopy(model)
+        self.module.eval()
+        self.decay = decay
+        self.device = device  # perform ema on different device from model if set
+        if self.device is not None:
+            self.module.to(device=device)
+
+    def _update(self, model, update_fn):
+        with torch.no_grad():
+            for ema_v, model_v in zip(self.module.state_dict().values(), model.state_dict().values()):
+                if self.device is not None:
+                    model_v = model_v.to(device=self.device)
+                ema_v.copy_(update_fn(ema_v, model_v))
+
+    def update(self, model):
+        self._update(model, update_fn=lambda e, m: self.decay * e + (1. - self.decay) * m)
+
+    def set(self, model):
+        self._update(model, update_fn=lambda e, m: m)
+
+
+# Cell
+class EMACallback(pl.callbacks.Callback):
+    """
+    Model Exponential Moving Average. Empirically it has been found that using the moving average
+    of the trained parameters of a deep network is better than using its trained parameters directly.
+
+    If `use_ema_weights`, then the ema parameters of the network is set after training end.
+    """
+
+    def __init__(self, decay=0.9999, use_ema_weights: bool = True):
+        self.decay = decay
+        self.ema = None
+        self.use_ema_weights = use_ema_weights
+
+    def on_fit_start(self, trainer, pl_module):
+        "Initialize `ModelEmaV2` from timm to keep a copy of the moving average of the weights"
+        self.ema = ModelEmaV2(pl_module, decay=self.decay, device=None)
+
+    def on_train_batch_end(
+        self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx=0
+    ):
+        "Update the stored parameters using a moving average"
+        # Update currently maintained parameters.
+        self.ema.update(pl_module)
+
+    def on_validation_epoch_start(self, trainer, pl_module):
+        "do validation using the stored parameters"
+        # save original parameters before replacing with EMA version
+        self.store(pl_module.parameters())
+
+        # update the LightningModule with the EMA weights
+        # ~ Copy EMA parameters to LightningModule
+        self.copy_to(self.ema.module.parameters(), pl_module.parameters())
+
+    def on_validation_end(self, trainer, pl_module):
+        "Restore original parameters to resume training later"
+        self.restore(pl_module.parameters())
+
+    def on_save_checkpoint(self, trainer, pl_module, checkpoint):
+        if self.ema is not None:
+            return {"state_dict_ema": self.ema.state_dict()}
+
+    def on_load_checkpoint(self, trainer, pl_module, checkpoint):
+        if self.ema is not None:
+            self.ema.module.load_state_dict(checkpoint["state_dict_ema"])
+
+    def store(self, parameters):
+        "Save the current parameters for restoring later."
+        self.collected_params = [param.clone() for param in parameters]
+
+    def restore(self, parameters):
+        """
+        Restore the parameters stored with the `store` method.
+        Useful to validate the model with EMA parameters without affecting the
+        original optimization process.
+        """
+        for c_param, param in zip(self.collected_params, parameters):
+            param.data.copy_(c_param.data)
+
+    def copy_to(self, shadow_parameters, parameters):
+        "Copy current parameters into given collection of parameters."
+        for s_param, param in zip(shadow_parameters, parameters):
+            if param.requires_grad:
+                param.data.copy_(s_param.data)
+
+    def on_train_end(self, trainer, pl_module):
+        # update the LightningModule with the EMA weights
+        if self.use_ema_weights:
+            self.copy_to(self.ema.module.parameters(), pl_module.parameters())
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--train-demonstrations", type=str, required=True)
@@ -315,6 +434,8 @@ def main():
     )
     parser.add_argument("--image-downsample", type=int, default=5)
     parser.add_argument("--patch-size", type=int, default=12)
+    parser.add_argument("--ema", action="store_true")
+    parser.add_argument("--ema-decay", type=float, default=0.995)
     args = parser.parse_args()
 
     exp_name = "gscan"
@@ -414,6 +535,14 @@ def main():
     logs_root_dir = f"{args.log_dir}/{exp_name}/{model_name}/{dataset_name}/{seed}"
     most_recent_version = args.version
 
+    callbacks = [
+        pl.callbacks.LearningRateMonitor(),
+        checkpoint_cb,
+    ]
+
+    if args.ema:
+        callbacks.append(EMACallback(decay=args.ema_decay))
+
     trainer = pl.Trainer(
         logger=[
             TensorBoardLogger(logs_root_dir, version=most_recent_version),
@@ -421,7 +550,7 @@ def main():
                 logs_root_dir, version=most_recent_version, flush_logs_every_n_steps=10
             ),
         ],
-        callbacks=[pl.callbacks.LearningRateMonitor(), checkpoint_cb],
+        callbacks=callbacks,
         max_steps=iterations,
         num_sanity_val_steps=10,
         accelerator="gpu" if torch.cuda.is_available() else None,
