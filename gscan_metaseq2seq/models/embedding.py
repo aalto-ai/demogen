d1a0cf689a50566836f0a8b4906b6d1fa2cc57ed
Sam Spilsbury <smspillaz@gmail.com>
2024-05-20 10:11:22 +0300

    embedding: Allow setting custom state component lengths

--- gscan_metaseq2seq/models/embedding.py
+++ gscan_metaseq2seq/models/embedding.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import numpy as np
 
 
 def initialize_parameters(m):
@@ -12,20 +13,31 @@ def initialize_parameters(m):
 
 
 class BOWEmbedding(nn.Module):
-    def __init__(self, max_value, n_channels, embedding_dim):
+    def __init__(self, max_value, n_channels, embedding_dim, state_component_lengths=None):
         super().__init__()
+        state_component_lengths = (
+            state_component_lengths if state_component_lengths is not None
+            else [max_value] * n_channels
+        )
+
         self.max_value = max_value
         self.embedding_dim = embedding_dim
-        self.embedding = nn.Embedding(n_channels * max_value, embedding_dim)
+        self.embedding = nn.Embedding(sum(state_component_lengths), embedding_dim)
         self.n_channels = n_channels
+        self.register_buffer(
+            "state_component_offsets",
+            torch.from_numpy(
+                np.cumsum([0] + (
+                    state_component_lengths
+                )[:-1])
+            ),
+            persistent=False
+        )
         self.apply(initialize_parameters)
 
     def forward(self, inputs):
         flat_inputs = inputs.flatten(0, -2)
-
-        offsets = torch.Tensor([i * self.max_value for i in range(self.n_channels)]).to(
-            inputs.device
-        )
+        offsets = self.state_component_offsets
         offsetted = (flat_inputs + offsets[None, :]).long()
         each_embedding = self.embedding(offsetted)
         each_embedding_flat = each_embedding.flatten(-2, -1)
