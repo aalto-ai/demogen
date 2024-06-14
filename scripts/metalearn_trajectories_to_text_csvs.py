f305a6a9e88cb5392f8e8324923376e470979174
Sam Spilsbury <smspillaz@gmail.com>
2024-06-14 20:25:19 +0300

    metalearn_trajectories_to_text: Support ReaSCAN and GSRR splits
    Also make sure that the baseline case gets state information

--- scripts/metalearn_trajectories_to_text_csvs.py
+++ scripts/metalearn_trajectories_to_text_csvs.py
@@ -10,7 +10,7 @@ import pprint
 import os
 from gscan_metaseq2seq.util.load_data import load_data_directories
 
-def encode_individual_state(s, idx2color, idx2object):
+def gscan_encode_individual_state(s, idx2color, idx2object):
     size, color, obj, agent, direction, x, y = s
 
     if agent == 1:
@@ -19,9 +19,27 @@ def encode_individual_state(s, idx2color, idx2object):
     return f"{idx2color[color - 1]} {idx2object[obj - 1]} s: {size} x: {x} y: {y}"
 
 
-def encode_state(state, idx2color, idx2object):
-    return ", ".join(map(lambda s: encode_individual_state(s, idx2color, idx2object),
-                         filter(lambda s: s[-1] != 0, state)))
+def reascan_gscan_encode_individual_state(s, idx2color, idx2object):
+    size, color, obj, agent, direction, x, y, box_size, box_color, is_box = s
+
+    if is_box == 1:
+        return f"{box_color} box s: {box_size} x: {x} y {y}"
+
+    if agent == 1:
+        return f"agent d: {direction} x: {x} y: {y}"
+
+    return f"{idx2color[color - 1]} {idx2object[obj - 1]} s: {size} x: {x} y: {y}"
+
+
+STATE_PROFILES = {
+    "gscan": gscan_encode_individual_state,
+    "reascan": reascan_gscan_encode_individual_state
+}
+
+
+def encode_state(state, idx2color, idx2object, state_profile):
+    return ", ".join(map(lambda s: STATE_PROFILES[state_profile](s, idx2color, idx2object),
+                         filter(lambda s: (s != 0).any(), state)))
 
 
 def encode_instr(instr, idx2word, word2idx):
@@ -32,8 +50,14 @@ def encode_targets(targets, idx2action, action2idx):
     return " ".join([idx2action[w] for w in targets if w != action2idx['[pad]']])
 
 
-def convert_to_text_representation(example, idx2word, idx2action, word2idx, action2idx, idx2color, idx2object, max_examples=16, reverse=False):
-    query_instr, query_targets, query_state, support_states, support_instrs, support_targets, scores = example
+def handle_is_metalearn(example):
+    if len(example) == 3:
+        return (*example, [], [], [], [])
+
+    return example
+
+def convert_to_text_representation(example, idx2word, idx2action, word2idx, action2idx, idx2color, idx2object, state_profile, max_examples=16, reverse=False):
+    query_instr, query_targets, query_state, support_states, support_instrs, support_targets, scores = handle_is_metalearn(example)
 
     scores = np.array(scores)
     rev_stride = -1 if reverse else 1
@@ -42,9 +66,9 @@ def convert_to_text_representation(example, idx2word, idx2action, word2idx, acti
     support_instrs = [support_instrs[i] for i in scores.argsort()[::rev_stride]]
     support_targets = [support_targets[i] for i in scores.argsort()[::rev_stride]]
 
-    if isinstance(support_states[0], np.ndarray):
-        state_line = f"State: {encode_state(query_state, idx2color, idx2object)}\n"
-        return ("Complete based on the following. Base the answer on Inputs Output pairs that are relevant to the Query Input:\n" + "\n".join([
+    if len(support_states) > 0 and isinstance(support_states[0], np.ndarray):
+        state_line = f"State: {encode_state(query_state, idx2color, idx2object, state_profile)}\n"
+        return ("Complete based on the following. Base the answer on Inputs Output pairs that are relevant to the Query Input:\n" + state_line + "\n".join([
             f"Input: {encode_instr(instr, idx2word, word2idx)}\nOutput: {encode_targets(targets, idx2action, action2idx)}"
             for instr, targets in zip(
                 support_instrs[-max_examples:],
@@ -55,13 +79,13 @@ def convert_to_text_representation(example, idx2word, idx2action, word2idx, acti
     support_states = [support_states[i] for i in scores.argsort()[::rev_stride]]
 
     return ("Complete based on the following. Base the answer on Input Output pairs that are relevant to the Query Input:\n" + "\n".join([
-        f"State: {encode_state(state, idx2color, idx2object)}\nInput: {encode_instr(instr, idx2word, word2idx)}\nOutput: {encode_targets(targets, idx2action, action2idx)}"
+        f"State: {encode_state(state, idx2color, idx2object, state_profile)}\nInput: {encode_instr(instr, idx2word, word2idx)}\nOutput: {encode_targets(targets, idx2action, action2idx)}"
         for state, instr, targets in zip(
             support_states[-max_examples:],
             support_instrs[-max_examples:],
             support_targets[-max_examples:]
         )
-    ]) + f"\nState: {encode_state(query_state, idx2color, idx2object)}\nQuery Input: {encode_instr(query_instr, idx2word, word2idx)}\nOutput:", encode_targets(query_targets, idx2action, action2idx))
+    ]) + f"\nState: {encode_state(query_state, idx2color, idx2object, state_profile)}\nQuery Input: {encode_instr(query_instr, idx2word, word2idx)}\nOutput:", encode_targets(query_targets, idx2action, action2idx))
 
 
 def print_through(val):
@@ -104,6 +128,12 @@ def check_already_done(path):
                           file_lines)))
 
 
+action_remap = {
+    'turn left': 'lturn',
+    'turn right': 'rturn'
+}
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--dataset", type=str, required=True, help="List of input sentences")
@@ -113,6 +143,7 @@ def main():
     parser.add_argument("--limit", type=int)
     parser.add_argument("--limit-examples", type=int, default=8)
     parser.add_argument("--reverse", action="store_true")
+    parser.add_argument("--state-profile", type=str, choices=STATE_PROFILES.keys(), required=True)
     args = parser.parse_args()
 
     (
@@ -127,36 +158,39 @@ def main():
 
     word2idx, action2idx, idx2color, idx2object = dictionaries
     idx2word = list(word2idx.keys())
-    idx2action = list(action2idx.keys())
-    idx2action = ['pull', 'push', 'stay', 'lturn', 'rturn', 'walk', '[pad]', '[sos]', '[eos]']
+    idx2action = list(map(lambda x: action_remap.get(x, x), action2idx.keys()))
     #idx2action = list(map(str, range(len(idx2action))))
     #idx2action[action2idx['[pad]']] = '[pad]'
     #action2idx = {a: i for i, a in enumerate(idx2action)} # 
 
     os.makedirs(args.output_responses, exist_ok=True)
 
-    for split_examples in {
+    for key, examples in {
         "train": train_demonstrations,
         **valid_demonstrations_dict
     }.items():
-        df = pd.DataFrame(
-            list(map(
-                            lambda e: convert_to_text_representation(
-                                e,
-                                idx2word,
-                                idx2action,
-                                word2idx,
-                                action2idx,
-                                idx2color,
-                                idx2object,
-                                args.limit_examples,
-                                args.reverse
-                            ),
-                            split_examples[1][:args.limit]
-            ))
-        )
-        df.columns = ["text", "label"]
-        df.to_csv(os.path.join(args.output_responses, f"{split_examples[0]}.tsv"), sep='\t')
+        if len(examples) == 0:
+            df = pd.DataFrame(columns=["text", "label"])
+        else:
+            df = pd.DataFrame(
+                list(map(
+                    lambda e: convert_to_text_representation(
+                        e,
+                        idx2word,
+                        idx2action,
+                        word2idx,
+                        action2idx,
+                        idx2color,
+                        idx2object,
+                        args.state_profile,
+                        args.limit_examples,
+                        args.reverse
+                    ),
+                    tqdm.tqdm(examples[:args.limit])
+                ))
+            )
+            df.columns = ["text", "label"]
+        df.to_csv(os.path.join(args.output_responses, f"{key}.tsv"), sep='\t')
 
 
 if __name__ == "__main__":
