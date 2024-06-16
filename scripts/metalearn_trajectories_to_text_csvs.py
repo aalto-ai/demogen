2237a57280403cae102d4562abf26d4a3f762340
Sam Spilsbury <smspillaz@gmail.com>
2024-06-16 16:55:20 +0300

    metalearn_trajectories_to_text_csvs: Improve text-encoding of states
    The dictionaries have to be sorted to get the right outputs
    and also x and y were flipped.

--- scripts/metalearn_trajectories_to_text_csvs.py
+++ scripts/metalearn_trajectories_to_text_csvs.py
@@ -11,7 +11,7 @@ import os
 from gscan_metaseq2seq.util.load_data import load_data_directories
 
 def gscan_encode_individual_state(s, idx2color, idx2object):
-    size, color, obj, agent, direction, x, y = s
+    size, color, obj, agent, direction, y, x = s
 
     if agent == 1:
         return f"agent d: {direction} x: {x} y: {y}"
@@ -20,10 +20,10 @@ def gscan_encode_individual_state(s, idx2color, idx2object):
 
 
 def reascan_gscan_encode_individual_state(s, idx2color, idx2object):
-    size, color, obj, agent, direction, x, y, box_size, box_color, is_box = s
+    size, color, obj, agent, direction, y, x, box_size, box_color, is_box = s
 
     if is_box == 1:
-        return f"{box_color} box s: {box_size} x: {x} y {y}"
+        return f"{idx2color[box_color - 1]} box s: {box_size} x: {x} y {y}"
 
     if agent == 1:
         return f"agent d: {direction} x: {x} y: {y}"
@@ -36,10 +36,15 @@ STATE_PROFILES = {
     "reascan": reascan_gscan_encode_individual_state
 }
 
+STATE_PROFILE_SORT = {
+    "gscan": lambda x: sorted(x, key=lambda y: (y[-2], y[-1])),
+    "reascan": lambda x: sorted(x, key=lambda y: (y[-5], y[-4])),
+}
+
 
 def encode_state(state, idx2color, idx2object, state_profile):
     return ", ".join(map(lambda s: STATE_PROFILES[state_profile](s, idx2color, idx2object),
-                         filter(lambda s: (s != 0).any(), state)))
+                         STATE_PROFILE_SORT[state_profile](list(filter(lambda s: (s != 0).any(), state)))))
 
 
 def encode_instr(instr, idx2word, word2idx):
@@ -78,14 +83,24 @@ def convert_to_text_representation(example, idx2word, idx2action, word2idx, acti
 
     support_states = [support_states[i] for i in scores.argsort()[::rev_stride]]
 
-    return ("Complete based on the following. Base the answer on Input Output pairs that are relevant to the Query Input:\n" + "\n".join([
+    state_str = f"State: {encode_state(query_state, idx2color, idx2object, state_profile)}\n"
+    query_input_str = f"Query Input: {encode_instr(query_instr, idx2word, word2idx)}"
+    supports_str = "\n".join([
         f"State: {encode_state(state, idx2color, idx2object, state_profile)}\nInput: {encode_instr(instr, idx2word, word2idx)}\nOutput: {encode_targets(targets, idx2action, action2idx)}"
         for state, instr, targets in zip(
             support_states[-max_examples:],
             support_instrs[-max_examples:],
             support_targets[-max_examples:]
         )
-    ]) + f"\nState: {encode_state(query_state, idx2color, idx2object, state_profile)}\nQuery Input: {encode_instr(query_instr, idx2word, word2idx)}\nOutput:", encode_targets(query_targets, idx2action, action2idx))
+    ])
+
+    return "\n".join([
+        "Complete based on the following. Base the answer on Input Output pairs that are relevant to the Query Input:",
+        supports_str,
+        state_str,
+        query_input_str,
+        "Output:"
+    ]), encode_targets(query_targets, idx2action, action2idx)
 
 
 def print_through(val):
@@ -162,6 +177,9 @@ def main():
     #idx2action = list(map(str, range(len(idx2action))))
     #idx2action[action2idx['[pad]']] = '[pad]'
     #action2idx = {a: i for i, a in enumerate(idx2action)} # 
+
+    idx2color = sorted(idx2color)
+    idx2object = sorted(idx2object)
 
     os.makedirs(args.output_responses, exist_ok=True)
 
